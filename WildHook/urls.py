8fe23a59cf4045c4c30aa642f213be5c2bea3f9e
JonnyDavison <jonnydavison@icloud.com>
2023-10-28 11:30:58 +0000

    Create urls.py for shop & create path, Add url path to WildHook urls.py

--- WildHook/urls.py
+++ WildHook/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import item_list
+
+app_name = 'shop'
+urlpatterns = [
+    path('', item_list, name='item-list')
+]

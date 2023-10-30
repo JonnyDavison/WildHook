47d7e364de7f5a68d8f5c94eaa09eb99ef37b2b5
JonnyDavison <jonnydavison@icloud.com>
2023-10-30 16:28:19 +0000

    Create larger Item class in shop models.py & migrate

--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,27 @@ from django.db import models
 from django.conf import settings
 
 
+CATEGORY_CHOICES = (
+    ('REEL', 'Reels'),
+    ('ROD', 'Rods'),
+    ('TACKLE', 'Tackle')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=10)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    description = models.TextField(null=True, blank=True)
+    slug = models.SlugField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title

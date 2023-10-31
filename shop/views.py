c9dc19cf1c4bbfc140ba6ee7cfd8f46a6f1dc508
JonnyDavison <jonnydavison@icloud.com>
2023-10-31 13:11:54 +0000

    Create ProductView in views.py & refactor item_list

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
+
+
+class ProductView(DetailView):
+    model = Item
+    shop_template = 'shop/item_detail.html'
 
 
 def item_list(request):
 
     items = Item.objects.all()
+    shop = 'shop/item_list.html'
+
     context = {
         'items': items
     }
-    return render(request, 'shop/item_list.html', context)
+    return render(request, shop, context)

70ebbc58750b0910dac1ae5baa3489be78097d47
JonnyDavison <jonnydavison@icloud.com>
2023-10-31 23:58:59 +0000

    Create add_to_cart path in urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,11 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import (ProductView)
+from .views import (
+    ProductView,
+    add_to_cart,
+    )
 
 app_name = 'shop'
 
 urlpatterns = [
     path('item_list', views.item_list, name='item_list'),
     path('product/<slug>/', ProductView.as_view(), name='product')
+    path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart')
+
 ]

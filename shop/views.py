45b9cdd05008db8822f8402ba37260510181a3eb
JonnyDavison <jonnydavison@icloud.com>
2023-11-01 00:27:11 +0000

    Fix in views.py, remove kwargs from redirect statement in add_to_cart

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from .models import Item, OrderItem, Order
+from django.utils import timezone
 
 
 class ProductView(DetailView):
@@ -29,8 +30,8 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
     else:
-        order = Order.objects.create(user=request.user)
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,
+                                     ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect('shop:product', kwargs={
-        'slug': slug
-    })
+    return redirect('shop:product', slug=slug)

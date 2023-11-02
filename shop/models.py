00c7454f049fea471a7baf6d2ea76534eff242bb
JonnyDavison <jonnydavison@icloud.com>
2023-11-02 14:04:09 +0000

    Create get_remove_from_cart_url allowing items to be removed

--- shop/models.py
+++ shop/models.py
@@ -48,6 +48,11 @@ class Item(models.Model):
             'slug': self.slug
         })
 
+    def get_remove_from_cart_url(self):
+        return reverse("shop:remove_from_cart", kwargs={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,

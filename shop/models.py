b26d4df63007fa12a4d264e1ce02a96024782bb6
JonnyDavison <jonnydavison@icloud.com>
2023-11-03 17:07:25 +0000

    Create functions to display savings, final price and total price

--- shop/models.py
+++ shop/models.py
@@ -69,7 +69,14 @@ class OrderItem(models.Model):
     
     def get_total_discount_item_price(self):
         return self.quantity * self.item.offer_price
+    
+    def get_amount_saved(self):
+        return round(self.get_total_item_price() - self.get_total_discount_item_price(), 2)
 
+    def get_final_price(self):
+        if self.item.offer_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -78,3 +85,12 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
+    def get_total(self):
+        total=0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total

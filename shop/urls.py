957bffd4f48094ac3690a9399efff2d7301fce3d
JonnyDavison <jonnydavison@icloud.com>
2023-11-03 20:43:57 +0000

    Update url path to allow items to be removed

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,7 @@ from .views import (
     OrderSummary,
     add_to_cart,
     remove_from_cart,
+    remove_single_item_from_cart,
     )
 
 app_name = 'shop'
@@ -17,5 +18,6 @@ urlpatterns = [
     path('order_summary', OrderSummary.as_view(), name='order_summary'),
     path('product/<slug>/', ProductView.as_view(), name='product'),
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
-    path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart')
+    path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart'),
+    path('remove_item_from_cart/<slug>/', remove_single_item_from_cart, name='remove_single_item_from_cart')
 ]

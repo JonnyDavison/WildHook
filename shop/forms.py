6306b506b8cff92f06e60b4b46ee04387582b6f0
JonnyDavison <jonnydavison@icloud.com>
2023-11-03 22:31:21 +0000

    Create checkout.html, create forms.py & create CheckoutForm class

---
+++ shop/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django_countries.fields import CountryField
+
+class CheckoutForm(forms.form):
+    street_address = forms.CharField()
+    street_address_2 = forms.CharField(required=False)
+    country = CountryField(blank_label='(select country)')
+    postcode = forms.CharField()
+    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
+    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    payment_option = forms.BooleanField(widget=forms.RadioSelect())

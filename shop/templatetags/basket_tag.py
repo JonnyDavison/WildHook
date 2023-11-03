a899c019901b01a5934c8e5c62d58f5edcfce618
JonnyDavison <jonnydavison@icloud.com>
2023-11-03 12:40:57 +0000

    Create template tags to allow basket to count number of items

--- shop/templatetags/basket_tag.py
+++ shop/templatetags/basket_tag.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.filter
 def basket_item_count(user):
     if user.is_authenticated:
-        q_set = Order.objects.filter(user=user, ordered=False)
-        if q_set.exists():
-            return q_set[0].items.count()
+        qs = Order.objects.filter(user=user, ordered=False)
+        if qs.exists():
+            return qs[0].items.count()
     return 0

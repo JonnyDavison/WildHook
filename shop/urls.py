4ecbd55c87283b28f2f997ac31366ad3ba59964d
JonnyDavison <jonnydavison@icloud.com>
2023-11-03 14:57:41 +0000

    Create order_summary url path to display the html page

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from . import views
 from .views import (
     ProductView,
+    OrderSummary,
     add_to_cart,
     remove_from_cart,
     )
@@ -13,6 +14,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('item_list', views.item_list, name='item_list'),
+    path('order_summary', OrderSummary.as_view(), name='order_summary'),
     path('product/<slug>/', ProductView.as_view(), name='product'),
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart')

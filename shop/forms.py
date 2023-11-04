a74406c41df352855c9794a42c5f1d4d0cb6acda
JonnyDavison <jonnydavison@icloud.com>
2023-11-04 01:35:46 +0000

    Update form.py to use template literals for form styling

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
 
 PAYMENT_CHOICES = (
     ('S', 'Stripe'),
@@ -7,11 +9,22 @@ PAYMENT_CHOICES = (
 )
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    street_address_2 = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
-    postcode = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main St'
+    }))
+    street_address_2 = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Address line 2'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(
+        widget=CountrySelectWidget(attrs={
+            'class': 'form-select d-block w-100',
+        }))
+    postcode = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    same_billing_address = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    save_info = forms.BooleanField(required=False, widget=forms.CheckboxInput())
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+    same_billing_address = forms.BooleanField(required=False)

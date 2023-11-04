c2de669d47c55362133403cd835afb8b7774b663
JonnyDavison <jonnydavison@icloud.com>
2023-11-04 00:38:48 +0000

    Add checkout path in urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,7 @@ from . import views
 from .views import (
     ProductView,
     OrderSummary,
+    CheckoutView,
     add_to_cart,
     remove_from_cart,
     remove_single_item_from_cart,
@@ -19,5 +20,6 @@ urlpatterns = [
     path('product/<slug>/', ProductView.as_view(), name='product'),
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart'),
-    path('remove_item_from_cart/<slug>/', remove_single_item_from_cart, name='remove_single_item_from_cart')
+    path('remove_item_from_cart/<slug>/', remove_single_item_from_cart, name='remove_single_item_from_cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
 ]

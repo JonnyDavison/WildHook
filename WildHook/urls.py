91dd95e2915464a4d7172e9490484e868c826fd1
JonnyDavison <jonnydavison@icloud.com>
2023-11-06 22:14:29 +0000

    Update payment path in project level urls

--- WildHook/urls.py
+++ WildHook/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('shop/', include('shop.urls')),
+    path('payment/', include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

d11a9f9633ea725da8803fc3609ee76c51f158e4
JonnyDavison <jonnydavison@icloud.com>
2023-11-09 20:19:06 +0000

    Update admin.py to display more info on th eadmin pannel

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import Item, OrderItem, Order
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'price',
+        'offer_price',
+        'image',
+    )
 
-admin.site.register(Item)
-admin.site.register(OrderItem)
-admin.site.register(Order)
+
+class OrderItemAdmin(admin.ModelAdmin):
+   list_display = (
+        'user',
+        'ordered',
+        'quantity',
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+   list_display = (
+        'user',
+        'ordered',
+        'ordered_date',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Order, OrderAdmin)

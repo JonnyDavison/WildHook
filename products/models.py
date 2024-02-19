7082bbaee4deea108b8eb75b1fcd0962f12fed6c
JonnyDavison <jonnydavison@icloud.com>
2024-02-19 21:49:23 +0000

    Create products models.py, migrate and register in admin.py

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+TAG_CHOICES = (
+    ('N', 'NEW'),
+    ('L', 'LOW STOCK'),
+    ('P', 'ON SALE'),
+    ('S', 'SOLD OUT')
+)
+
+
+class Category(models.Model):
+    """ Category model """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """ Product model """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    offer_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    tag = models.CharField(choices=TAG_CHOICES, max_length=1)
+    slug = models.SlugField(max_length=254, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

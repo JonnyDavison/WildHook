ecc4ad1cee4b41463afeb191bf5ec45f8f89f8f5
JonnyDavison <jonnydavison@icloud.com>
2024-02-20 08:42:10 +0000

    Update Admin panel, to display relevant info when logged in

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """ Organise the Products admin panel """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'tag',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ Organise the Category admin panel """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)

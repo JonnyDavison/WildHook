571df617d9a181e0794207412556fff84c129dee
JonnyDavison <jonnydavison@icloud.com>
2024-02-22 08:27:38 +0000

    Create View for product detail

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -14,3 +14,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request):
+    """
+    A view to return Product detail page
+    """
+
+    product = get_oject_of_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)

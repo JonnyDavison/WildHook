db574cf81de72711619e42f3c6e36e0272a2c236
JonnyDavison <jonnydavison@icloud.com>
2024-02-27 08:37:06 +0000

    Add hook sizing if has sizes

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,27 +14,43 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
     saving = 0
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        offer_price = product.offer_price
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            offer_price = product.offer_price
 
-        if offer_price is not None:
-            sub_total = offer_price * quantity
-            saving = (product.price * quantity) - (product.offer_price * quantity)
+            if offer_price is not None:
+                sub_total = offer_price * item_data
+                saving = (product.price * item_data) - (product.offer_price * item_data)
+            else:
+                sub_total = total
+
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'offer_price': offer_price,
+                'sub_total': sub_total,
+                'saving': saving
+            })
         else:
-            sub_total = total
-
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            'offer_price': offer_price,
-            'sub_total': sub_total,
-            'saving': saving
-        })
-
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'offer_price': offer_price,
+                    'sub_total': sub_total,
+                    'saving': saving,
+                    'size': size
+                })
+ 
+ 
     if total < settings.FREE_DELIVERY:
         delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY - total

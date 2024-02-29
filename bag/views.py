a1aeda6534e76a258403ad09bff8fff0c259f078
JonnyDavison <jonnydavison@icloud.com>
2024-02-29 21:35:43 +0000

    Add message trigers tot he view to send toasts to user

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def bag(request):
@@ -11,6 +13,7 @@ def bag(request):
 def add_to_bag(request, item_id):
     """ Adds the selected quantity of items to the bag"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -22,15 +25,23 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added {size.upper()} {product.name} to your bag')
+
         else:
             bag[item_id] = {'items_by_size': { size: quantity}}
+            messages.success(request, f'Added {size.upper()} {product.name} to your bag')
+
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.info(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)

1d214ee7dccb7c5e13d832d48b2c82154dfa7037
JonnyDavison <jonnydavison@icloud.com>
2024-03-06 15:19:12 +0000

    Update Models & Migrate to allow larger line item totals offer_price

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY:
             self.delivery_cost = self.order_total * settings.DELIVERY_PERCENTAGE / 100
         else:
@@ -57,19 +57,24 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     """ Model to hold Product and order information """
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # String of hook sizes ordered
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False,
+                                blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True,
+                                    blank=True) # String of hook sizes ordered
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
-        super().save(*args, **kwargs)
+        if not self.lineitem_total:
+            price_to_use = self.product.offer_price if self.product.offer_price is not None else self.product.price
+            self.lineitem_total = price_to_use * self.quantity
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'

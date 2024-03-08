1051c7e60dcca4f57def6491f75907d1c7679073
JonnyDavison <jonnydavison@icloud.com>
2024-03-08 09:38:39 +0000

    Create view to cache the data from checkbox

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
@@ -7,6 +8,23 @@ from .models import OrderLineItem, Order
 
 from bag.contexts import bag_contents
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    """ Save customer info"""
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "Sorry there was an issue processing your payment")
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -28,9 +46,13 @@ def checkout(request):
             'postcode': request.POST['postcode'],
         }
         order_form = OrderForm(form_data)
-        
+
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)

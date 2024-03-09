f256318df0ad77966c97e5474f55a9bbdc35e41a
JonnyDavison <jonnydavison@icloud.com>
2024-03-09 10:02:16 +0000

    Create order history view, url and table to display

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -25,4 +26,20 @@ def profile(request):
         'on_profile_page': True
     }
 
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """Display order history"""
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request (
+        f'Past confirmation Order Number {order_number}.'
+        'A confirmation email was sent at the time of ordering.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
+    }
     return render(request, template, context)

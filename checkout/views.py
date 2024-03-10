53a2eed3f459c2f43469a05595bfb7f80faf6f35
JonnyDavison <jonnydavison@icloud.com>
2024-03-10 09:29:43 +0000

    Upadte views to prefill & save user info and attach to order

--- checkout/views.py
+++ checkout/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpR
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-from .forms import OrderForm
-from products.models import Product
-from .models import OrderLineItem, Order
 
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
+
 import stripe
 import json
 
@@ -34,6 +38,7 @@ def checkout(request):
 
     if request.method =='POST':
         bag = request.session.get('bag', {})
+        
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -45,8 +50,8 @@ def checkout(request):
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
         }
-        order_form = OrderForm(form_data)
 
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
@@ -64,7 +69,7 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].item():
+                        for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -72,7 +77,6 @@ def checkout(request):
                                 product_size=size,
                             )
                             order_line_item.save()
-
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the Products you ordered is out of stock or doesn't exist."
@@ -83,11 +87,9 @@ def checkout(request):
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
-
         else:
             messages.error(request, "There was an problem with your form \
                 please double check your info and try again.")
-
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -103,8 +105,25 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
-    order_form = OrderForm()
     if not stripe_public_key:
         messages.warning(request, "Set your stripe key in your environment")
 
@@ -123,9 +142,31 @@ def checkout_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach user profile to order
+        order.user_profile = profile
+        order.save()
+
+        # Save user info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Your Order has been Successfully Processed! \
         your order number is {order_number}. A confirmation \
-            email is on to the way to {order.email}.')
+        email is on to the way to {order.email}.')
     if 'bag' in request.session:
         del request.session['bag']
 

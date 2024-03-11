c984f6d2090cbe3b028fd767e00ad55ca05af66a
JonnyDavison <jonnydavison@icloud.com>
2024-03-11 07:49:37 +0000

    Create superuser access to add/edit/delete products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Product, Category
 from django.db.models.functions import Lower
@@ -10,7 +11,7 @@ def all_products(request):
     """
     A view to return all Products page
     """
-
+    
     products = Product.objects.all()
     query = None
     categories = None
@@ -53,15 +54,19 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
-
+@login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, "Please see admin to gain approved access")
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed, please check form is valid.')
     else:
@@ -73,3 +78,43 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product """
+    if not request.user.is_superuser:
+        messages.error(request, "Please see admin to gain approved access")
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product Updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Product Update Failed. Please check the form and retry.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product """
+    if not request.user.is_superuser:
+        messages.error(request, "Please see admin to gain approved access")
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted.')
+    return redirect(reverse('products'))

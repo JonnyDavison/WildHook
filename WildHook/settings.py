6b53b85f9763a22955c65483ebedb7b6468ef593
JonnyDavison <jonnydavison@icloud.com>
2024-03-11 09:16:13 +0000

    Update DEBUG state

--- WildHook/settings.py
+++ WildHook/settings.py
@@ -25,10 +25,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jonnydavison-wildhook-9pxva2zv1o4.ws-eu108.gitpod.io', 'wildhook-72f0fa2bfc47.herokuapp.com']
 

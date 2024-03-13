b787405b1cad53172f863f9a1cfe478e98c0e732
JonnyDavison <jonnydavison@icloud.com>
2024-03-13 14:35:28 +0000

    Update Models and migrate

--- WildHook/urls.py
+++ WildHook/urls.py
@@ -7,18 +7,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('summernote/', include('django_summernote.urls')),
 
     path('shop/', include('shop.urls')),  #
     path('payment/', include('payment.urls')),  #
 
+    path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
-    
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'WildHook.views.handler404'
--- blog/models.py
+++ blog/models.py
@@ -4,36 +4,31 @@ from django.utils.text import slugify
 from django.urls import reverse
 
 
-class Category(models.Model):
-    """ Categories for organising blog posts """
 
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+CATEGORY_CHOICES = (
+    ('NEWS', 'News'),
+    ('FISHING_TRIPS', 'Fishing Trips'),
+    ('TACKLE', 'Tackle')
+)
 
-    def __str__(self):
-        return self.name
-
-
-class Tag(models.Model):
-    """ Tags for categorising model """
-
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.name
-
+TAG_CHOICES = (
+    ('ORVIS', 'Orvis'),
+    ('SAVAGE_GEAR', 'Savage Gear'),
+    ('SHIMANO', 'Shimano'),
+    ('SAGE', 'Sage')
+)
 
 class Post(models.Model):
     """ Blog post model """
 
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, blank=True)
     publication_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='blog_images/')
+    image = models.ImageField(null=True, blank=True)
     content = models.TextField()
-    categories = models.ManyToManyField(Category)
-    tags = models.ManyToManyField(Tag)
+    tags = models.CharField(choices=TAG_CHOICES, max_length=20, null=True, blank=True)
+    categories = models.CharField(choices=CATEGORY_CHOICES, max_length=20, null=True, blank=True)
     slug = models.SlugField(unique=True, blank=True)
     meta_description = models.CharField(max_length=200, blank=True)
     meta_keywords = models.CharField(max_length=200, blank=True)

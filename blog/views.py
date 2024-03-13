2e778f50714fc68fdc07db9ac21e503c88388bcd
JonnyDavison <jonnydavison@icloud.com>
2024-03-13 14:39:00 +0000

    Add post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Tag
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
 
 def post_list(request):
-    """ Display a list of all blog posts """
+    """View to display a list of all blog posts."""
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+    context = {
+        'posts': posts
+        }
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, slug):
+    """View to display a detailed view of a blog post."""
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post})

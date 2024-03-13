08a9cdc62788905f0083ed85a4061bccfca627b5
JonnyDavison <jonnydavison@icloud.com>
2024-03-13 20:16:59 +0000

    Create ability to make and edit posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('post_list', views.post_list, name='post_list'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create_post', views.create_post, name='create_post'),
+    path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -15,5 +18,46 @@ def post_list(request):
 def post_detail(request, slug):
     """View to display a detailed view of a blog post."""
     post = get_object_or_404(Post, slug=slug)
+    meta_keywords = post.meta_keywords 
+    meta_description = post.meta_description 
     queryset = Post.objects.filter()
-    return render(request, 'blog/post_detail.html', {'post': post})
+    
+    context = {
+        'post': post,
+        'meta_keywords': meta_keywords,
+        'meta_description': meta_description,
+        
+        }
+    return render(request, 'blog/post_detail.html', context)
+
+@login_required
+def create_post(request):
+    """View to create a new blog post."""
+    if not request.user.is_superuser:
+        return redirect('post_list')  # Redirect if user is not a superuser
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/create_post.html', {'form': form})
+
+@login_required
+def edit_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if not request.user.is_superuser:
+        # If not a superuser, return a message & redirect
+        messages.error(request, "You do not have permission to access this page.")
+        return redirect('post_list')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit_post.html', {'form': form, 'post': post})

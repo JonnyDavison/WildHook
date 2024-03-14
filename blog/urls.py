078761f51464156b9f2374d34ed2a2586f69889e
JonnyDavison <jonnydavison@icloud.com>
2024-03-14 20:24:23 +0000

    Update urls after runnignautoamted testing

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post_list', views.post_list, name='post_list'),
+    path('post_list/', views.post_list, name='post_list'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('create_post', views.create_post, name='create_post'),
+    path('create_post/', views.create_post, name='create_post'),
     path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
     path('post/delete/<slug:slug>/', views.delete_post, name='delete_post'),
 ]
